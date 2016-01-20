8c5ee4a29aa3d75c994e6255c4a57cd4ed8d72ea
Tom Robertshaw <me@tomrobertshaw.net>
2016-01-20 20:48:17 +0000

    Add original file and converted file columns to podcasts table.

---
+++ database/migrations/2016_01_20_204521_addPostcastFileNames.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPostcastFileNames extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('podcasts', function (Blueprint $table) {
+            $table->string('original_file');
+            $table->string('converted_file');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
