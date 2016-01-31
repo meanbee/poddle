04277e17dd29b6b11ac74d4e308444b906fadec0
Tom Robertshaw <me@tomrobertshaw.net>
2016-01-31 13:44:45 +0000

    Provide layout and start homepage.

---
+++ resources/views/cms/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title', 'Home')
+@section('description', 'Podcast transcript service and indexing service')
+
+@section('content')
+<section class="main-inner">
+    <div class="pocket-login-promo">
+        <h1>Podcast Show Notes</h1>
+        <p>Bringing accessibility to Podcasts with transcripts, concept summary and search indexing</p>
+</section>
+@stop
+@section('before_body_end')
+@stop
