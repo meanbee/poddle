fddc44fa18f481c27f5ea64fece1924bdbfcc864
Tom Robertshaw <me@tomrobertshaw.net>
2016-01-31 14:53:55 +0000

    Add podcast submission to homepage.

--- resources/views/cms/home.blade.php
+++ resources/views/cms/home.blade.php
@@ -7,6 +7,9 @@
 <section class="main-inner">
     <div class="homepage-promo">
         <p>Bringing accessibility to Podcasts with transcripts, concept summary and search indexing</p>
+
+        @include('podcast/submit')
+    </div>
 </section>
 @stop
 @section('before_body_end')
