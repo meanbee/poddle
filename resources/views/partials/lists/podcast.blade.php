8d0cf41e4d16eb3750d5a0a99ba28c4560a0417f
Tom Robertshaw <me@tomrobertshaw.net>
2016-02-10 08:19:10 +0000

    Replace podcast list partial with getters

--- resources/views/partials/lists/podcast.blade.php
+++ resources/views/partials/lists/podcast.blade.php
@@ -1,9 +1,9 @@
 <ul class="podcast-episodes-list podcast-episodes-recent">
     @foreach($recentPodcasts as $podcast)
         <li class="podcast-episode-item">
-            <a class="podcast-episode-link" href="{{ route('podcast.view', ['id' => $podcast->getId(), 'slug' => $podcast->getSlug()]) }}">{{ $podcast->episode_name }}</a>
-            <p class="podcast-episode-name">{{ $podcast->podcast_name }}</p>
-            <span class="podcast-episode-published-date">{{ $podcast->published_date->diffForHumans() }}</span>
+            <a class="podcast-episode-link" href="{{ route('podcast.view', ['id' => $podcast->getId(), 'slug' => $podcast->getSlug()]) }}">{{ $podcast->getEpisodeName() }}</a>
+            <p class="podcast-episode-name">{{ $podcast->getPodcastName() }}</p>
+            <span class="podcast-episode-published-date">{{ $podcast->getPublishedDate()->diffForHumans() }}</span>
         </li>
     @endforeach
 </ul>
