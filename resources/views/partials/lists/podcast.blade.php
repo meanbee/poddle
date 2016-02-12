2f19babb38ddf1ba31e80634226d201705d01ad6
Tom Robertshaw <me@tomrobertshaw.net>
2016-02-12 17:16:12 +0000

    Clear transcription container.

--- resources/views/partials/lists/podcast.blade.php
+++ resources/views/partials/lists/podcast.blade.php
@@ -8,7 +8,7 @@
             @if ($podcast->getPublishedDate())
             <span class="podcast-episode-published-date">{{ $podcast->getPublishedDate()->diffForHumans() }}</span>
             @endif
-                
+
             <a class="podcast-episode-link" href="{{ route('podcast.view', ['id' => $podcast->getId(), 'slug' => $podcast->getSlug()]) }}">{{ $podcast->getEpisodeName() }}</a>
         </li>
     @endforeach
